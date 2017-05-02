7bec2fad4adb62f294bdd3f5e7a2b0be241884a6
aashtonnz <aashtonnz@gmail.com>
2017-05-03 00:25:27 +1200

    Update linear_regression_closed_form.py

--- linear_regression_closed_form.py
+++ linear_regression_closed_form.py
@@ -4,8 +4,13 @@ import matplotlib.pyplot as plt
 
 # Train/test split
 SPLIT = 0.7
+
 # Number of sample predictions to show
 SAMPLE = 20
+
+# ------------------------------------------------------------------------------
+# Get data
+# ------------------------------------------------------------------------------
 
 # Get data field
 df = pd.read_csv("happiness.csv")
@@ -24,6 +29,10 @@ y_train = train[:, [-1]]
 x_test = test[:, :-1]
 y_test = test[:, [-1]]
 
+# ------------------------------------------------------------------------------
+# Run algorithm
+# ------------------------------------------------------------------------------
+
 # Train
 theta = np.linalg.inv(x_train.T.dot(x_train)).dot(x_train.T).dot(y_train)
 h = lambda x: x.dot(theta)
@@ -34,12 +43,22 @@ y_pred = h(x_test)
 # Get average error
 avg_error = (sum((y_pred - y_test) ** 2) / (2.0 * len(y_pred)))[0]
 
+# ------------------------------------------------------------------------------
+# Show results
+# ------------------------------------------------------------------------------
+
+# Print data field
 print df
 print "\n"
-print np.c_[y_test, y_pred, y_test - y_pred][:SAMPLE]
+
+# Print sample predictions against test outputs
+print np.c_[y_test, y_pred][:SAMPLE]
 print "\n"
+
+# Print average error
 print avg_error
 
+# Plot test outputs in red and predictions in blue against first input feature
 plt.plot(x_test[:, 1], y_test[:, 0], 'ro')
 plt.plot(x_test[:, 1], y_pred[:, 0], 'bo')
 plt.show()
